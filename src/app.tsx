ce83065e006dd9f7b278db6420e6b0379c3364ce
YameenShariz <yameenshariz2006@gmail.com>
2025-12-25 07:04:21 +0530

    Project page removed
    The project page is now replaced with the committee page.
    The project, news, and resources page have been commented off

--- src/app.tsx
+++ src/app.tsx
@@ -9,10 +9,10 @@ import Committee from "./pages/Committee";
 import Events from "./pages/Events";
 import EventDetails from "./pages/EventDetails";
 import Membership from "./pages/Membership";
-import Projects from "./pages/Projects";
-import Resources from "./pages/Resources";
+// import Projects from "./pages/Projects";
+// import Resources from "./pages/Resources";
 import Gallery from "./pages/Gallery";
-import News from "./pages/News";
+// import News from "./pages/News";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
@@ -31,10 +31,10 @@ const App = () => (
           <Route path="/events" element={<Events />} />
           <Route path="/events/:slug" element={<EventDetails />} />
           <Route path="/membership" element={<Membership />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/resources" element={<Resources />} />
+          {/* <Route path="/projects" element={<Projects />} /> */}
+          {/* <Route path="/resources" element={<Resources />} /> */}
           <Route path="/gallery" element={<Gallery />} />
-          <Route path="/news" element={<News />} />
+          {/* <Route path="/news" element={<News />} /> */}
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
